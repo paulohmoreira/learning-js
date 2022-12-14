2d8ebf66a473d31b22d2673432bb8f27d8fee47e
Paulo Moreira <phmoreira37@gmail.com>
2022-12-13 21:47:08 -0300

    Closures

---
+++ closures.js
@@ -0,0 +1,12 @@
+// Função se lembra do contexto em que ela foi criada
+function soma(x) {
+  return (y) => {
+    return x + y;
+  };
+}
+
+const somaParcial = soma(10);
+
+console.log(somaParcial(20));
+console.log(somaParcial(30));
+console.log(somaParcial(40));
