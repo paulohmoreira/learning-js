f253d96610a625fdce9409e856400b460cf07cbb
Paulo Moreira <phmoreira37@gmail.com>
2023-01-01 19:07:38 -0300

    Manipulando arquivo e convertendo para objeto

--- promises/index.js
+++ promises/index.js
@@ -6,5 +6,15 @@ const promiseArquivo = fs.promises.readFile(filePath);
 
 promiseArquivo
   .then((arquivo) => arquivo.toString('utf-8'))
-  .then((texto) => console.log(texto))
+  .then((textoDoArquivo) => textoDoArquivo.split('\n').slice(1))
+  .then((linhasSemCabecalho) =>
+    linhasSemCabecalho.map((linha) => {
+      const [nome, feito] = linha.split(';');
+      return {
+        nome,
+        feito: feito.trim() === 'true',
+      };
+    })
+  )
+  .then((listaDeTarefas) => console.log(listaDeTarefas))
   .catch((error) => console.log('Deu ruim!', error));
