325b438313a2d42fd046fe8c60359d5b6cbfccfd
Paulo Moreira <phmoreira37@gmail.com>
2023-01-08 22:52:06 -0300

    função som para todas as teclas

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -2,5 +2,21 @@ function playSound() {
   document.querySelector('#som_tecla_pom').play();
 }
 
-const key = document.querySelector('.tecla_pom');
-key.onclick = playSound;
+const pom = document.querySelector('.tecla_pom');
+const clap = document.querySelector('.tecla_clap');
+const tim = document.querySelector('.tecla_tim');
+const puff = document.querySelector('.tecla_puff');
+const splash = document.querySelector('.tecla_splash');
+const toim = document.querySelector('.tecla_toim');
+const psh = document.querySelector('.tecla_psh');
+const tic = document.querySelector('.tecla_tic');
+const tom = document.querySelector('.tecla_tom');
+pom.onclick = playSound;
+clap.onclick = playSound;
+tim.onclick = playSound;
+puff.onclick = playSound;
+splash.onclick = playSound;
+toim.onclick = playSound;
+psh.onclick = playSound;
+tic.onclick = playSound;
+tom.onclick = playSound;
