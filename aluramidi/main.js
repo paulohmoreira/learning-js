4e48332153dd525bd87c27350ae496c3e2b12a72
Paulo Moreira <phmoreira37@gmail.com>
2023-01-14 21:51:59 -0300

    refatorado código utilizando o while

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,22 +1,17 @@
-function playSound() {
-  document.querySelector('#som_tecla_pom').play();
+function playSound(keyId) {
+  document.querySelector(keyId).play();
 }
 
-const pom = document.querySelector('.tecla_pom');
-const clap = document.querySelector('.tecla_clap');
-const tim = document.querySelector('.tecla_tim');
-const puff = document.querySelector('.tecla_puff');
-const splash = document.querySelector('.tecla_splash');
-const toim = document.querySelector('.tecla_toim');
-const psh = document.querySelector('.tecla_psh');
-const tic = document.querySelector('.tecla_tic');
-const tom = document.querySelector('.tecla_tom');
-pom.onclick = playSound;
-clap.onclick = playSound;
-tim.onclick = playSound;
-puff.onclick = playSound;
-splash.onclick = playSound;
-toim.onclick = playSound;
-psh.onclick = playSound;
-tic.onclick = playSound;
-tom.onclick = playSound;
+const keyList = document.querySelectorAll('.tecla');
+
+let i = 0;
+while (i < keyList.length) {
+  const key = keyList[i];
+  const keyClass = key.classList[1];
+  const keyId = `#som_${keyClass}`;
+
+  key.onclick = () => {
+    playSound(keyId);
+  };
+  i++;
+}
