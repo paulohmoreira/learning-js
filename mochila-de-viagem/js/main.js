b1df1e3877f41a4491f43af0a337cc0e8d0d7a8c
Paulo Moreira <phmoreira37@gmail.com>
2023-01-23 17:35:16 -0300

    adicionado botao remover elemento

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -46,7 +46,10 @@ function criaElemento(item) {
   numeroItem.dataset.id = item.id;
 
   novoItem.appendChild(numeroItem);
+
   novoItem.innerHTML += item.nome;
+
+  novoItem.appendChild(botaoDeleta());
 
   lista.appendChild(novoItem);
 }
@@ -54,4 +57,19 @@ function criaElemento(item) {
 function atualizaElemento(item) {
   document.querySelector('[data-id="' + item.id + '"]').innerHTML =
     item.quantidade;
+}
+
+function botaoDeleta() {
+  const elementoBotao = document.createElement('button');
+  elementoBotao.innerText = 'X';
+
+  elementoBotao.addEventListener('click', function () {
+    deletaElemento(this.parentNode);
+  });
+
+  return elementoBotao;
+}
+
+function deletaElemento(tag) {
+  tag.remove();
 }
