3fbc1b3c300f91f3936ee8c071c7ec2fbfe75463
Paulo Moreira <phmoreira37@gmail.com>
2023-01-23 17:24:57 -0300

    verificando se item existe

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -10,14 +10,26 @@ form.addEventListener('submit', (e) => {
   let nome = e.target.elements['nome'];
   let quantidade = e.target.elements['quantidade'];
 
+  const existe = itens.find((element) => element.nome === nome.value);
+
   const itemAtual = {
     nome: nome.value,
     quantidade: quantidade.value,
   };
 
-  criaElemento(itemAtual);
+  if (existe) {
+    itemAtual.id = existe.id;
 
-  itens.push(itemAtual);
+    atualizaElemento(itemAtual);
+
+    itens[existe.id] = itemAtual;
+  } else {
+    itemAtual.id = itens.length;
+
+    criaElemento(itemAtual);
+
+    itens.push(itemAtual);
+  }
 
   localStorage.setItem('itens', JSON.stringify(itens));
 
@@ -31,9 +43,15 @@ function criaElemento(item) {
 
   const numeroItem = document.createElement('strong');
   numeroItem.innerHTML = item.quantidade;
+  numeroItem.dataset.id = item.id;
 
   novoItem.appendChild(numeroItem);
   novoItem.innerHTML += item.nome;
 
   lista.appendChild(novoItem);
+}
+
+function atualizaElemento(item) {
+  document.querySelector('[data-id="' + item.id + '"]').innerHTML =
+    item.quantidade;
 }
