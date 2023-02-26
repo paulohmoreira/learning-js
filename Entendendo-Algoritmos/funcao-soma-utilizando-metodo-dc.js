3f63b59ef24ccd4c37356f0dd4ae08a34779029a
Paulo Moreira <phmoreira37@gmail.com>
2023-02-26 16:52:26 -0300

    Nota: Esta foi a resposta retirada do livro. É uma solução muito mais limpa e legível do que o meu código anterior.

--- Entendendo-Algoritmos/funcao-soma-utilizando-metodo-dc.js
+++ Entendendo-Algoritmos/funcao-soma-utilizando-metodo-dc.js
@@ -1,22 +1,13 @@
+/*
+  A função começa verificando se a lista está vazia. Se for o caso, ela retorna 
+  o valor 0. Caso contrário, ela soma o primeiro elemento da lista com o 
+  resultado da chamada recursiva da função somaRecursiva() na lista restante 
+  (ou seja, a lista sem o primeiro elemento).
+*/
 const somaRecursiva = (listaDeNumeros) => {
   // Verificar se a lista está vazia. Se for o caso retorna o valor 0.
   if (listaDeNumeros.length === 0) return 0;
-
-  // Fazer uma cópia do array original.
-  let copiaListaDeNumeros = listaDeNumeros.slice();
-
-  /*
-    Verifica se a lista tem um único elemento.
-    Se for o caso, a função retorna o único elemento da lista.
-    Caso contrário, a função remove o primeiro elemento da lista com o método 
-    shift(), adiciona-o à soma dos elementos restantes da lista, que é 
-    calculada chamando recursivamente a função soma() na lista atualizada.
-  */
-  if (copiaListaDeNumeros.length === 1) return copiaListaDeNumeros[0];
-  else {
-    let primeiroNumero = copiaListaDeNumeros.shift();
-    return primeiroNumero + somaRecursiva(copiaListaDeNumeros);
-  }
+  return listaDeNumeros[0] + somaRecursiva(listaDeNumeros.slice(1));
 };
 
 const listaDeNumeros = [1, 2, 3, 4];
