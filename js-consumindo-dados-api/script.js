813f719a21149b8337d48dd9ee7653c749048828
Paulo Moreira <phmoreira37@gmail.com>
2023-04-02 15:50:01 -0300

    Fetch API - Async Await

--- js-consumindo-dados-api/script.js
+++ js-consumindo-dados-api/script.js
@@ -1,9 +1,23 @@
 // Callback Hell
-const cosultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-  .then((resposta) => resposta.json())
-  .then((r) => {
-    if (r.erro) throw Error('Esse cep não existe!');
-    else console.log(r);
-  })
-  .catch((erro) => console.log(erro))
-  .finally((mensagem) => console.log('Processamento concluído'));
+// const cosultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+//   .then((resposta) => resposta.json())
+//   .then((r) => {
+//     if (r.erro) throw Error('Esse cep não existe!');
+//     else console.log(r);
+//   })
+//   .catch((erro) => console.log(erro))
+//   .finally((mensagem) => console.log('Processamento concluído'));
+
+// Async e await
+async function buscaEndereco() {
+  try {
+    const cosultaCEP = await fetch('https://viacep.com.br/ws/01001230/json/');
+    const consultaConvertida = await cosultaCEP.json();
+    if (consultaConvertida.erro) throw Error('CEP não existente!');
+    console.log(consultaConvertida);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+buscaEndereco();
