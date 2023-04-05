50c2022518306c49ccee7f4fa0083d908baa0219
Paulo Moreira <phmoreira37@gmail.com>
2023-04-05 08:54:32 -0300

    Lazy Loading

---
+++ lazy-loading/src/main.js
@@ -0,0 +1,24 @@
+const images = document.querySelectorAll('[data-src]');
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: '0px 0px 300px 0px',
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+    preloadImage(entry.target);
+    imgObserver.unobserve(entry.target);
+  });
+}, imgOptions);
+
+images.forEach((image) => {
+  imgObserver.observe(image);
+});
+
+function preloadImage(img) {
+  const src = img.getAttribute('data-src');
+  if (!src) return;
+  img.src = src;
+}
